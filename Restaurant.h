f168138ec399e3a87aa2d653dbd3de8aa60c0435
Dyue1004 <39129523+Dyue1004@users.noreply.github.com>
2018-05-10 00:28:26 +0930

    Update Restaurant.h

--- Restaurant.h
+++ Restaurant.h
@@ -2,12 +2,5 @@
 #define RESTAURANT_H
 
 using namespace std;
-
+kam is a loser 
 class Restaurant
-{
-  public:
-         Restaurant();
-         int date;
-         int time;
-         int number;
-         
